affa03592f2e0efb9e650b361de3d9e9a1102a96
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-12-19 01:36:00 +0600

    reg normal validation complete need to proper validate like(regex)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,29 @@
+const userSchema = require("../models/userSchema");
 
-const registration =(req,res)=>{
+const registration = async(req,res)=>{
 
 
 
     const {fullName,email,password}=req.body;
 
     if (!fullName) return res.status(400).send({messege:'FullName is required'})
+    if (!email) return res.status(400).send({messege:'email is required'})
+    if (!password) return res.status(400).send({messege:'password is required'})
+
+        const existingUSer =  await userSchema.findOne({email});
+
+        if(existingUSer) return res.status(400).send({messege:'User Already exist'})
+
+            const newUser = new userSchema({
+                fullName,
+                email,
+                password
+            })
     
+          await   newUser.save()
+
+
+          res.status(201).send({messege:'Registration Successfull'})
 }
 
 
