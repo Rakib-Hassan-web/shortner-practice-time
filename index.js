d1108b321245c7e7d4ae890a4a1891ef6ac0a062
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-12-18 23:40:29 +0600

    server runnign

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(8000, () => {
+  console.log(`Server Running on port 8000 `)
+})
