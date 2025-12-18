d952e93c4d7dc070cfcb27143a078d4145088e7a
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-12-19 00:10:46 +0600

    server running

---
+++ dbConfig/index.js
@@ -0,0 +1,12 @@
+
+
+const mongoose = require('mongoose');
+
+const DATABASECONNECT =()=>{
+    return mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
+
+}
+
+
+module.exports= DATABASECONNECT
