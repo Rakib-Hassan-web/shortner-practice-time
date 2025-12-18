74791bf32a55029e352ce4d889dbe0d6329824ac
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-12-19 01:51:52 +0600

    login validation on process

--- controllers/authController.js
+++ controllers/authController.js
@@ -33,8 +33,27 @@ const registration = async(req,res)=>{
 
 
 
-const login =(req,res)=>{
-res.send('Any ')
+const login = async(req,res)=>{
+
+
+    const {email,password}=req.body;
+
+    if (!email) return res.status(400).send({messege:'FullName is required  '})
+    if (!isvalidEmail(email)) return res.status(400).send({messege:'Please Enter a valid email'})
+         if (!password) return res.status(400).send({messege:'password is required'})
+    if (!isvalidPassword(password)) return res.status(400).send({messege:' Invalid password '})
+
+
+        const existingUSer = await userSchema.findOne({email})
+
+        if(!existingUSer) return res.status(400).send({messege:'No User Found In this Email'})
+
+
+
+
+            res.status(200).send({messege:' Login su '})
+
+
 }
 
 
