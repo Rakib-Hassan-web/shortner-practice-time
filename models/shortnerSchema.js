da7ba158e4631e5db90288acfe579b1f0434ce1a
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-12-20 00:18:04 +0600

    shortner schema

---
+++ models/shortnerSchema.js
@@ -0,0 +1,35 @@
+
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+
+const ShortnerSchema = new Schema({
+
+    LongUrl:{
+        type:String,
+        required: true,
+    },
+     ShortUrl:{
+        type:String,
+        required: true,
+    },
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:"allusers"
+    },
+    VisitHistory:[
+        {
+            VisitTime:{
+                type:Date,
+                default:Date.now()
+            }
+        }
+    ]
+},{timestamps:true});
+
+
+
+module.exports=
