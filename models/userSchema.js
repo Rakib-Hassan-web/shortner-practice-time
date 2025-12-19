cc0216467c5177279b5a8f39c928c844be9aedf8
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-12-19 23:53:04 +0600

    pass hash practice done

--- models/userSchema.js
+++ models/userSchema.js
@@ -49,13 +49,7 @@ UserSchema.methods.ComparePAss = async function (Candidatepass) {
 
 
 
-// UserSchema.pre('save' ,async function () {
-//     if(!this.isModified('password')) return
 
-//     const salt = await bcrypt.genSalt(10)
-
-//     this.password =await bcrypt.hash(this.password ,salt)
-// })
 
 
 // UserSchema.methods.ComparePass = async function (candidatePass) {
